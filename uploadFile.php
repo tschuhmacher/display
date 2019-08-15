24e056bd2891521907b7aebebadf1c75edb2281d
tks <t.schuhmacher@gmail.com>
2019-08-15 16:07:35 +0200

    as it is.

---
+++ uploadFile.php
@@ -0,0 +1,29 @@
+<?php
+
+    //file extension of sended file
+    $fileExtension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+
+    //extension for image
+    $imageExtensions = ['jpg', 'jpeg', 'png'];
+
+    //path where will be file uploaded
+	$path = !in_array($fileExtension, $imageExtensions) ? __DIR__.'/'.'models/' : __DIR__.'/'.'img/';
+	echo $path;
+
+
+    //allowed file extension for simple validation
+    $allowedExtensions = ['jpg', 'jpeg', 'png', 'json', 'gltf'];
+
+    //validation of file size and type
+    if (!in_array($fileExtension, $allowedExtensions)) {
+        http_response_code(422);
+    }
+
+    //upload file to folder
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $path . $_FILES["file"]['name'])) {
+        http_response_code(200);
+    } else {
+        http_response_code(503);
+    }
+
+?>
